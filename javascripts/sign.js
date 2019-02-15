775d4272646604b1fc28730eb9977b7aa988322d
emilpalsson <emil@emilpalsson.se>
2019-02-15 20:27:10 +0100

    Add acknowledgement

---
+++ javascripts/sign.js
@@ -0,0 +1,5 @@
+var sign = "                 _ _             _\n   ___ _ __ ___ (_) |_ __   __ _| |___ ___  ___  _ __\n  / _ \\ '_ ` _ \\| | | '_ \\ / _` | / __/ __|/ _ \\| '_ \\\n |  __/ | | | | | | | |_) | (_| | \\__ \\__ \\ (_) | | | |\n  \\___|_| |_| |_|_|_| .__/ \\__,_|_|___/___/\\___/|_| |_|\n                    |_|\n                                         @emilpalsson\n                                      /in/emilpalsson\n                       https://github.com/emilpalsson";
+var original = "\n\n\nOriginal game by Oz Elentok\nhttps://github.com/ozelentok/SpaceInvaders";
+var fork = "\n\nArdaland fork available at\nhttps://github.com/emilpalsson/ardaland-spaceinvaders";
+var license = "\n\nMIT license\nhttps://ardaland.svt.se/LICENSE.txt";
+console.log(sign + original + fork + license);
